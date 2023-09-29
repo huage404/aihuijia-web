7d73f4c46b0577fe012f7735931fb936de6a420d
Micah Xia <1962408497@qq.com>
2023-09-30 02:01:34 +0800

    feat: 添加 pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-
+    modules: [
+        '@pinia/nuxt'
+    ],
     build: {
         transpile:
             process.env.NODE_ENV === 'production'
---
+++ stores/user.ts
@@ -0,0 +1,15 @@
+import {defineStore} from 'pinia';
+
+const useUserStore = defineStore('user', {
+    state: () => ({
+
+    }),
+
+    actions: {
+        async login() {
+
+        }
+    }
+})
+
+export default useUserStore;
