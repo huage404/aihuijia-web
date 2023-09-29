cf5c0ae5d2f95381444382228ec425952483b82d
Micah Xia <1962408497@qq.com>
2023-09-30 00:36:34 +0800

    feat: 添加 ui 库：tailwindcss 、naiveUI

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+import 'vfonts/Lato.css';
+import 'vfonts/FiraCode.css';
+
+import {NConfigProvider, GlobalThemeOverrides, zhCN, dateZhCN} from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#FF0000'
+  },
+  Button: {
+    textColor: '#FF0000'
+  }
+}
+
+useHead({
+  meta: [{name: 'naive-ui-style'}]
+})
+
+</script>
+
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <n-config-provider
+        preflight-style-disabled
+        :locale="zhCN"
+        :date-locale="dateZhCN"
+        :theme-overrides="themeOverrides"
+    >
+      <NuxtPage/>
+      <n-global-style/>
+    </n-config-provider>
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+
+    build: {
+        transpile:
+            process.env.NODE_ENV === 'production'
+                ? [
+                    'naive-ui',
+                    'vueuc',
+                    '@css-render/vue3-ssr',
+                    '@juggle/resize-observer'
+                ]
+                : ['@juggle/resize-observer']
+    },
+    vite: {
+        optimizeDeps: {
+            include:
+                process.env.NODE_ENV === 'development'
+                    ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+                    : []
+        }
+    },
+    css: ['~/assets/styles/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
